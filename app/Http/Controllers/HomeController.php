a3ae2b741d66507bde08c56ac7479b151befa35f
Jose-Eduardo-PC <joseduardopati@gmail.com>
2021-08-10 20:18:19 -0300

    tercer commit controladores y vistas

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+         /*return view('welcome');*/
+    return view('home'); 
+    }
+}
